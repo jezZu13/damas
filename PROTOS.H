f7ea91fc82ffefc8f8d12b3621c96147d1dfd3c0
jezZu13 <jezzu1311@gmail.com>
2019-01-15 22:11:07 +0100

    version final

---
+++ PROTOS.H
@@ -0,0 +1,17 @@
+int mtest(void);
+
+void mver(void);
+
+void mocultar(void);
+
+int mposx(int modo);
+
+int mposy(int modo);
+
+void msituar(int modo, int x, int y);
+
+int mclick(void);
+
+void mlimitar(int modo,int x1, int y1, int x2, int y2);
+
+int inbox(int left,int top, int right, int bottom, int x, int y);
